f6ec99fd9a1b482b6fd5cc08d95142c82b49391f
Ion Munteanu <71140604+munteanuion@users.noreply.github.com>
2026-04-07 23:52:25 +0300

    Update IUCCInputsWrapper.cs

--- Runtime/UCC/Scripts/Input/IUCCInputsWrapper.cs
+++ Runtime/UCC/Scripts/Input/IUCCInputsWrapper.cs
@@ -1,7 +1,15 @@
+
+using UnityEngine;
+
 namespace UniversalCharacterController.Scripts
 {
     public interface IUCCInputsWrapper
     {
         public UCCInputData InputData { get; }
+        
+        public void MoveInput(Vector2 newMoveDirection);
+        public void LookInput(Vector2 newLookDirection);
+        public void JumpInput(bool newJumpState);
+        public void SprintInput(bool newSprintState);
     }
 }
